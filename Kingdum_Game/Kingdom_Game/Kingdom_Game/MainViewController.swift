b679953f36772e2a1b6b6088742e832ad7f68385
naoto kojima <>
2021-02-25 13:18:51 +0900

    Xcode_Project_開始

---
+++ Kingdum_Game/Kingdom_Game/Kingdom_Game/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kingdom_Game
+//
+//  Created by naoto kojima on 2021/02/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
